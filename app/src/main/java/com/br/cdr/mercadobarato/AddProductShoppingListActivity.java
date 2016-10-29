3815b18635adeac754976a3b1c109be16d08178c
cleberson <clebrsonn@hotmail.com>
2016-10-29 15:51:20 -0300

    adicionar a lista e novo produto

---
+++ app/src/main/java/com/br/cdr/mercadobarato/AddProductShoppingListActivity.java
@@ -0,0 +1,13 @@
+package com.br.cdr.mercadobarato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddProductShoppingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_shopping_list);
+    }
+}
