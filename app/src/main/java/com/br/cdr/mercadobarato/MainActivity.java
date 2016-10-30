445cf858febeb2235a8810514979f59a82d4025a
Rodrigojgxm <Rodrigojgxm91@gmail>
2016-10-30 11:23:51 -0300

    Login/Register/app_bar activities

--- app/src/main/java/com/br/cdr/mercadobarato/MainActivity.java
+++ app/src/main/java/com/br/cdr/mercadobarato/MainActivity.java
@@ -1,13 +1,46 @@
 package com.br.cdr.mercadobarato;
 
+import android.content.Intent;
+import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private Button login;
+    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MultiDex.install(this);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.main_bar);
+        setSupportActionBar(toolbar);
+
+        login  = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp = (Button) findViewById(R.id.btn_signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
+
